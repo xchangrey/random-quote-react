b0c30fdf4ad4f58b0a7ba294ba99ab03255d021b
xchangrey <geroychristian@gmail.com>
2022-10-24 19:16:11 +0800

    deletes serviceWorker

--- src/index.js
+++ src/index.js
@@ -18,8 +18,3 @@ root.render(
   </StrictMode>,
 );
 
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
